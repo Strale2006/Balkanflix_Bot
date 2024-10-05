e0a4f59cd4d5d5961cfcd6b84815006e651ba8d7
Strale2006 <straleisara@gmail.com>
2024-10-05 12:50:15 +0200

    Bot

--- bot.js
+++ bot.js
@@ -6,6 +6,15 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Function to generate a random series of numbers
+const generateRandomSeries = (length = 5, max = 100) => {
+    const series = [];
+    for (let i = 0; i < length; i++) {
+        series.push(Math.floor(Math.random() * max));
+    }
+    return series.join(', '); // Convert array to string
+};
+
 // Listen for messages
 client.on('messageCreate', message => {
     // Ignore messages from the bot itself
@@ -15,24 +24,29 @@ client.on('messageCreate', message => {
     if (message.content === '!ping') {
         message.channel.send('Pong!');
     }
-
     // Respond to the !hello command
     else if (message.content === '!hello') {
         message.channel.send(`Hello, ${message.author.username}!`);
     }
-
     // Respond to the !info command
     else if (message.content === '!info') {
         message.channel.send('This is a Discord bot created using Discord.js!');
     }
+    // Respond to the !random command
+    else if (message.content === '!random') {
+        const randomSeries = generateRandomSeries(); // Generate a random series
+        message.channel.send(`Random series: ${randomSeries}`);
+    }
 });
 
 // Log in to Discord with your app's token
-client.login('MTI5MjA0MTM3OTUwOTQzNjQzNw.G8VFar.rXMRZGwVXZOg-2pZopg-Fd7Wh1NAKbdIWJkL3Y').then(() => {
-    console.log('Bot logged in successfully');
-}).catch(err => {
-    console.error('Error logging in:', err);
-});
+client.login('MTI5MjA0MTM3OTUwOTQzNjQzNw.G8VFar.rXMRZGwVXZOg-2pZopg-Fd7Wh1NAKbdIWJkL3Y')
+    .then(() => {
+        console.log('Bot logged in successfully');
+    })
+    .catch(err => {
+        console.error('Error logging in:', err);
+    });
 
 // Define the serverless function
 module.exports = (req, res) => {
